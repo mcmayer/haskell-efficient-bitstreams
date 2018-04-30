1f0610523760a31f618cc032cd9ff9b6a7680498
MM <mmayer@mayeranalytics.com>
2018-04-30 15:15:34 +0200

    cleanup

--- Length.hs
+++ Length.hs
@@ -1,17 +1,9 @@
 module Main where
 
-    import           Data.Bits            (shiftR, (.&.))
-    import qualified Data.ByteString.Lazy as BSL
-    import           Data.Word8           (Word8)
-    
-    splitByte :: Word8 -> [Bool]
-    splitByte w = map (\i-> (w `shiftR` i) .&. 1 == 1) [0..7]
-    
-    bitStream :: BSL.ByteString -> [Bool]
-    bitStream bs = concat $ map splitByte (BSL.unpack bs)
-    
-    main :: IO ()
-    main = do
-        bs <- BSL.getContents
-        print $ BSL.length bs
-    
+import qualified Data.ByteString.Lazy as BSL
+
+main :: IO ()
+main = do
+    bs <- BSL.getContents
+    print $ BSL.length bs
+
--- sumBytes.hs
+++ sumBytes.hs
@@ -1,14 +1,6 @@
 module Main where
 
-import           Data.Bits            (shiftR, (.&.))
 import qualified Data.ByteString.Lazy as BSL
-import           Data.Word8           (Word8)
-
-splitByte :: Word8 -> [Bool]
-splitByte w = map (\i-> (w `shiftR` i) .&. 1 == 1) [0..7]
-
-bitStream :: BSL.ByteString -> [Bool]
-bitStream bs = concat $ map splitByte (BSL.unpack bs)
 
 main :: IO ()
 main = do
