3f0a343e5026d96e3c8571d0a8adbcb451a0ac31
MM <mmayer@mayeranalytics.com>
2018-06-27 08:15:31 +0200

    clean up

--- LongestSeqVStream.hs
+++ LongestSeqVStream.hs
@@ -5,71 +5,41 @@
 #define INLINE_INNER INLINE PHASE_INNER
 module Main where
 
-import           Control.Monad.Identity            (Identity)
 import           Data.Bits                         (shiftR, (.&.))
-import qualified Data.ByteString                   as BS
 import qualified Data.ByteString.Lazy              as BSL
 import qualified Data.Vector.Fusion.Stream.Monadic as S
 import           Data.Word8                        (Word8)
 
-
-
--- {-# INLINE mkWord8Stream #-}
-mkWord8Stream :: Monad m => BSL.ByteString -> S.Stream m Word8
-mkWord8Stream = S.Stream step where
-    -- {-# INLINE_INNER step #-}
-    step bs = case (BSL.uncons bs) of
-                Nothing        -> return S.Done
-                Just (w, bs'') -> return $ S.Yield w bs''
-
--- {-# INLINE mkBits #-}
-mkBits :: Monad m => Word8 -> S.Stream m Bool
-mkBits w' = S.Stream step (w', 8::Word8) where
-    {-# INLINE_INNER step #-}
-    step (w,n) | n==0 = return S.Done
-               | otherwise = return $ S.Yield (w .&. 1 == 1) (w `shiftR` 1, n-1)
-
 data Step = Step BSL.ByteString !Word8 !Word8
 
--- {-# INLINE mkBitstream' #-}
-mkBitstream' :: Monad m => BSL.ByteString -> S.Stream m Bool
-mkBitstream' bs' = S.Stream step (Step bs' 0 0) where
+mkBitstream :: Monad m => BSL.ByteString -> S.Stream m Bool
+mkBitstream bs' = S.Stream step (Step bs' 0 0) where
     {-# INLINE_INNER step #-}
     step (Step bs w n) | n==0 = case (BSL.uncons bs) of
                             Nothing        -> return S.Done
-                            Just (w', bs') -> return $ S.Yield (w' .&. 1 == 1) (Step bs' (w' `shiftR` 1) 7)
-                       | otherwise = return $ S.Yield (w .&. 1 == 1) (Step bs (w `shiftR` 1) (n-1))
-
--- {-# INLINE mkBitstream #-}
-mkBitstream :: Monad m => BSL.ByteString -> S.Stream m Bool
-mkBitstream bs = S.concatMap mkBits (mkWord8Stream bs)
+                            Just (w', bs') -> return $
+                                S.Yield (w' .&. 1 == 1) (Step bs' (w' `shiftR` 1) 7)
+                       | otherwise = return $
+                                S.Yield (w .&. 1 == 1) (Step bs (w `shiftR` 1) (n-1))
 
 data LongestRun = LongestRun !Bool !Int !Int
 
--- {-# INLINE extendRun #-}
+{-# INLINE extendRun #-}
 extendRun :: LongestRun -> Bool -> LongestRun
 extendRun (LongestRun previous run longest) x  = LongestRun x current (max current longest)
     where
     current = if x == previous then run + 1 else 1
 
--- {-# INLINE extendRun' #-}
-extendRun' :: Bool -> LongestRun -> LongestRun
-extendRun' x (LongestRun previous run longest) = LongestRun x current (max current longest)
-    where
-    current = if x == previous then run + 1 else 1
-
--- {-# INLINE longestRun #-}
+{-# INLINE longestRun #-}
 longestRun :: S.Stream IO Bool -> IO Int
 longestRun s = do
     (LongestRun _ _ longest) <- S.foldl' extendRun (LongestRun False 0 0) s
-    -- (LongestRun _ _ longest) <- S.foldr extendRun' (LongestRun False 0 0) s
+    -- (LongestRun _ _ longest) <- S.foldr (flip extendRun) (LongestRun False 0 0) s
     return longest
 
 main :: IO ()
 main = do
-    -- l <- S.toList $ mkBitstream' (BSL.pack [1,2])
-    -- print l
     bs <- BSL.getContents
-    l <- longestRun $ mkBitstream' bs
+    l <- longestRun $ mkBitstream bs
     print l
 
